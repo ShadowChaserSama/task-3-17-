25facdcfc0a9ccff0f0386454843a37e139b5566
Əli Zülbalayev <74174830+ShadowChaserSama@users.noreply.github.com>
2026-03-17 16:01:14 +0400

    Create T.py

---
+++ T.py
@@ -0,0 +1,10 @@
+ls = []
+
+with open('adamlar.csv','r') as a:
+  L = list(a)
+  for i in range(1,len(L)-1):
+    ls.append(L[i].split(',')[2].strip('\n'))
+
+ls = sorted(ls,reverse=True)
+
+print(ls)
